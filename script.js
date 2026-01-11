af7a1a0e62b936445ece31a038f2cc345cf8e87d
Labib-M-Aziz <148260195+Labib-M-Aziz@users.noreply.github.com>
2026-01-11 13:50:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,21 @@
-// CONFIG FIREBASE
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "ISI_API_KEY",
-  authDomain: "ISI_AUTH_DOMAIN",
-  projectId: "ISI_PROJECT_ID"
+  apiKey: "AIzaSyCp3MnR7JoaqJYeV-wIvya-lm5dsk_AUUU",
+  authDomain: "pengumumanbemsinergi.firebaseapp.com",
+  projectId: "pengumumanbemsinergi",
+  storageBucket: "pengumumanbemsinergi.firebasestorage.app",
+  messagingSenderId: "964275161508",
+  appId: "1:964275161508:web:25cb59e51fca2ddf221931",
+  measurementId: "G-0G21S8YRQY"
 };
 
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
-
-// FUNGSI CEK NIM
-function cekNIM() {
-  const nim = document.getElementById("nim").value;
-  const result = document.getElementById("result");
-
-  if (!nim) {
-    result.innerHTML = "⚠️ NIM tidak boleh kosong";
-    result.style.color = "orange";
-    return;
-  }
-
-  db.collection("peserta_bem")
-    .where("nim", "==", nim)
-    .get()
-    .then(snapshot => {
-      if (snapshot.empty) {
-        result.innerHTML = "❌ Data tidak ditemukan";
-        result.style.color = "red";
-        return;
-      }
-
-      snapshot.forEach(doc => {
-        const data = doc.data();
-
-        result.innerHTML = `
-          <p>Nama: <b>${data.nama}</b></p>
-          <p>Prodi: <b>${data.prodi}</b></p>
-          <p>Status: <b>${data.status}</b></p>
-        `;
-
-        result.style.color = data.status === "LOLOS" ? "green" : "red";
-      });
-    })
-    .catch(err => {
-      result.innerHTML = "Terjadi kesalahan";
-      console.error(err);
-    });
-}
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
